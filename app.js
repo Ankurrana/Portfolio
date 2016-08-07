53abaf59190b920fedf3fd4229318937c7fc1107
Ankur rana <ankurofficial@hotmail.com>
2016-08-07 20:36:12 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// app.use('/public',express.static(__dirname + "/public"));
+
+app.use('/resume',function(req,res){
+	res.download('./public/AnkurRanaCV.pdf','AnkurRana_CV.pdf')
+})
+
+app.use('/',function(req,res){
+	res.sendFile( path.join(__dirname,'/public/index.html'));
+})
+
+if(!process.env.PORT){
+	PORT = process.env.PORT;
+}else{
+	PORT = 3002
+}
+
+app.listen(PORT,function(){
+	console.log('Started listening to Port ' + PORT );
+});
