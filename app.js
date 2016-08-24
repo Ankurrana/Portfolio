fe84cce7bf5eb15091cc9b98d9dc178c7cb8b1cc
Ankur rana <ankurofficial@hotmail.com>
2016-08-24 23:20:51 +0530

    changed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var config = require(path.resolve(__dirname,'./config.js'))
+var config = require(__dirname+'/config.js')
 
 // app.use('/public',express.static(__dirname + "/public"));
 app.set('view engine','jade')
